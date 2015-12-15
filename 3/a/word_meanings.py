1f8cd83861f8b0a2c2a59a07510d7f21c6e356f6
Gautam B <t-gauba@microsoft.com>
2015-12-15 13:08:25 +0530

    Word dictionary
    Create a dictionary for words and their meanings. Write functions to add
    a new entry (word:meaning) , search for a particular word and retrieve
    meaning, given meaning find words with same meaning , remove an entry,
    display all words sorted alphabetically. [Program must be menu driven]

---
+++ 3/a/word_meanings.py
@@ -0,0 +1,32 @@
+__author__ = 'gauth_000'
+dictionary = {}
+
+
+def add_entry(entry):
+    dictionary[entry[0]] = dictionary.get(entry[0], [])
+    dictionary[entry[0]].append(entry[1])
+
+
+def search(word):
+    return ', '.join(dictionary.get(word, word + " doesn't exist"))
+
+
+def synonyms(phrase):
+    return ', '.join([k for k, v in dictionary.items() if phrase in v])
+
+
+def display():
+    for k in sorted(dictionary):
+        print(k, dictionary[k])
+
+
+add_entry(('a', 'b'))
+add_entry(('b', 'b'))
+add_entry(('c', 'd'))
+add_entry(('e', 'f'))
+add_entry(('c', 'f'))
+add_entry(('t', 'f'))
+add_entry(('i', 'j'))
+display()
+print(search('c'))
+print(synonyms('f'))
