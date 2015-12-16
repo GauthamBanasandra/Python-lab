9410035adcbabca9c5f354e997189d78e507a1df
Gautam B <t-gauba@microsoft.com>
2015-12-16 12:38:31 +0530

    Fixed the regex
    Now it can match multiple occurrences of a word anywhere in the string.

--- 8/b/string_processing.py
+++ 8/b/string_processing.py
@@ -1,9 +1,14 @@
 import re
+
 __author__ = 'gauth_000'
-string='1This is 1multiline string. It has many many lines. Well, many many is not too many. It is just 4lines.'
-# for match in re.findall(r'\b(\w+)\b.*?\1', string):
-#     print(match)
-match=re.findall(r'[^aeiou ]', string)
+string = '''1this is a string. is it not a string?
+it is nice'''
+print('multiple occurances of a word -')
+for match in re.finditer(r'\b(\w+)\b(?=.*?\1)', string, flags=re.DOTALL):
+    print(match.group(), match.span())
+print('consonants -')
+match = re.findall(r'[^aeiou ]', string, flags=re.DOTALL)
 print(''.join(match))
-match=re.match(r'_|\d.*', string)
+print('beginning with _ or digit -')
+match = re.match(r'_|\d.*?\s', string, flags=re.DOTALL)
 print(match)
