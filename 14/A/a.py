09459e4e8ab028042e1932084bdd95ac2b2bc14d
Gautam B <t-gauba@microsoft.com>
2015-12-16 11:57:02 +0530

    Prime number
    Write a Generator function which gives the next prime number each time.
    Use this to find all prime numbers in a given range.

--- 14/A/a.py
+++ 14/A/a.py
@@ -1,1 +1,19 @@
 __author__ = 'gauth_000'
+
+
+def next_prime(a, b):
+    while a <= b:
+        if is_prime(a):
+            yield a
+        a += 1
+
+
+def is_prime(num):
+    for i in range(2, num):
+        if not num % i:
+            return False
+    return True
+
+
+primes = next_prime(2, 10)
+print(list(primes))
